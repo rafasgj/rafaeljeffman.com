807f4b9077e12f86fb6feaf1a4cd348a22547a68
Rafael Guterres Jeffman <rafasgj@gmail.com>
2023-08-13 22:28:26 -0300

    Add list of classes and better output to new_lecture.sh

--- _utils/new_lecture.sh
+++ _utils/new_lecture.sh
@@ -1,19 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 usage() {
-    echo "usage: new_lecture.sh NICK NAME [TITLE] [YEAR] [INSTITUTION]"
+    echo "usage: new_lecture.sh NICK [TITLE] [YEAR] [NAME] [INSTITUTION]"
 }
 
-[ "$#" -lt 2 ] || [ "$#" -gt 5 ] && usage && exit 1
+[ "$#" -lt 1 ] || [ "$#" -gt 5 ] && usage && exit 1
+
+declare -A lectures=(
+    [analise-algoritmos]="Complexidade de Algoritmos e Análise de Desempenho"
+    [engsw]="Laboratório de Engenharia de Software"
+    [sistemas-distribuidos]="Sistemas Distribuídos"
+    [mobile]="Desenvolmimento para Dispositivos Móveis"
+    [compiler]="Compiladores"
+    [automata]="Linguagens Formais e Autômatos"
+)
 
 nick=$1
-name=$2
-title=${3:-''}
-year=${4:-$(date +"%Y")}
+title=${2:-''}
+year=${3:-$(date +"%Y")}
+name=${4:-${lectures[${nick}]}}
 institution=${5:-"Universidade LaSalle Canoas"}
 
 BASEDIR="$(dirname $0)/.."
-LECTURE_DIR="${BASEDIR}/teaching/lasalle/lectures/${nick}"
+LECTURE="teaching/lasalle/lectures/${nick}"
+LECTURE_DIR="${BASEDIR}/${LECTURE}"
 
 next_lecture=$({ echo "lecture-00.md" ; ls -1 "${LECTURE_DIR}"; } | tail -n 1 | xargs basename -s .md | cut -d- -f2)
 
@@ -23,3 +33,5 @@ source="${BASEDIR}/_utils/templates/lecture.md"
 [ -f "${BASEDIR}/_utils/templates/${next_lecture}" ] && source="${BASEDIR}/_utils/templates/${next_lecture}"
 
 sed -e "s/@TITLE@/${title}/g" -e "s/@DISCIPLINA@/${name}/g" -e "s/@YEAR@/${year}/g" -e "s/@INSTITUTION@/${institution}/g" -e "s/@NICK@/${nick}/g" < "${source}" > "${LECTURE_DIR}/${next_lecture}"
+
+echo "Created lecture: ${LECTURE}/${next_lecture}"
