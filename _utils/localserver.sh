8a1cbee420f38cb494d856a7c3eb6dce82440e23
Rafael Guterres Jeffman <rafasgj@gmail.com>
2024-06-19 23:20:30 -0300

    _utils/localserver: fix volume options on Linux or Mac

--- _utils/localserver.sh
+++ _utils/localserver.sh
@@ -7,6 +7,14 @@ TAG="$(whoami)/site_server"
 
 [ -d "${TOPDIR}/.vendor/bundle" ] || mkdir -p "${TOPDIR}/.vendor/bundle"
 
+PLATFORM="$(uname)"
+
+if [ "$PLATFORM" == "Linux" ]
+then
+    VOLOPT="Z"
+else
+    VOLOPT="rw"
+fi
 
 existing=$(podman images -f reference="localhost/${TAG}" --format "{{ .Repository }}")
 
@@ -17,8 +25,8 @@ then
 fi
 
 podman run \
-    --volume "${TOPDIR}:/srv/jekyll:rw" \
-    --volume "${TOPDIR}/.vendor/bundle:/usr/local/bundle:rw" \
+    --volume "${TOPDIR}:/srv/jekyll:${VOLOPT}" \
+    --volume "${TOPDIR}/.vendor/bundle:/usr/local/bundle:${VOLOPT}" \
     -p ${1:-4000}:4000 \
     "${TAG}"
 
